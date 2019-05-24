2b192b17c5e05daf4ff6ac9780bc7c6f43b3ec05
Michał <michaljanpatyk@gmail.com>
2019-05-24 14:14:55 +0200

    Initial commit

---
+++ web/hideshow.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('.button').click(function(evt){
+        evt.preventDefault();
+
+        var id = $(this).data('id');
+        console.log(id);
+        $('.selected').hide().removeClass('selected');
+        $('.' + id).addClass('selected').show();
+    });
+    $('li.change').hide();
+    $('.home').addClass('selected').show();
+});
