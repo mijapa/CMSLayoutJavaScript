3dc45987d4d9fc1ad0283fb28f9a4f565c02d06a
Michał <michaljanpatyk@gmail.com>
2019-05-25 11:05:54 +0200

    Add user Profile bean body

--- src/beans/Profile.java
+++ src/beans/Profile.java
@@ -4,4 +4,8 @@ import lombok.Data;
 
 @Data
 public class Profile {
+    private String first_name = "Nie podano";
+    private String last_name = "Nie podano";
+    private String company = "Nie podano";
+    private String business = "Nie podano";
 }
